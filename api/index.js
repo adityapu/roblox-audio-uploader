6bac8da6a74ebc89443c6654a381be0fa3f760f9
adityapu <43860986+adityapu@users.noreply.github.com>
2026-09-17 14:36:26 +0700

    Allow GET method in CORS settings
    Updated CORS settings to allow GET requests and added a response for GET method.

--- api/index.js
+++ api/index.js
@@ -4,7 +4,15 @@ const FormData = require('form-data');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-roblox-api-key');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  if (req.method === 'GET') {
+    return res.status(200).json({
+      status: 'OK',
+      service: 'Roblox Audio Uploader',
+      version: '2.0'
+    });
+  }
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
